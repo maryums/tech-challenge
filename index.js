c312777b5bd7badeb875395ae75491039b46de80
maryums <maryum@gmail.com>
2022-06-03 13:31:24 -0400

    moved events to utils.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { mockAPI, renderCards } from './utils/utils.js'
+import { mockAPI, renderCards, navEventListeners, linksEventListeners, renderArticle } from './utils/utils.js'
 
 // window.addEventListener('scroll', function () {
 //     const nav = document.querySelector('nav');
@@ -6,72 +6,5 @@ import { mockAPI, renderCards } from './utils/utils.js'
 // })
 
 console.log(mockAPI)
-
-document.body.addEventListener('click', function (event) {
-    console.log(event.target.id)
-    let index = (event.target.id) - 1
-
-    if (event.target.className === 'title-link') {
-
-        let cardContainer = document.querySelector(".card-container")
-        let header = document.querySelector("header")
-        let container = document.querySelector(".container")
-        container.removeChild(cardContainer)
-        container.removeChild(header)
-
-        let dataArr = mockAPI.data.posts
-
-        console.log(dataArr[index])
-        let articleContainer = document.createElement("div")
-        articleContainer.classList.add("article-container")
-
-        let articleImage = document.createElement("div")
-        articleImage.classList.add("article-image")
-        articleImage.innerHTML = `<img src="./assets/${event.target.id}.jpeg">`
-
-        let articleTitle = document.createElement("div")
-        articleTitle.classList.add("article-title")
-        articleTitle.innerHTML = `<h3> ${dataArr[index].title} </h3>`
-
-        let articleSummary = document.createElement("div")
-        articleSummary.classList.add("article-summary")
-        articleSummary.innerHTML = `<p> ${dataArr[index].article} </p>`
-
-        let articleDate = document.createElement("p")
-        articleDate.classList.add("article-date")
-        articleDate.innerHTML = "June 2, 2022"
-        let articleLinksUl = document.querySelector("#article-ul")
-        let articleLinks = document.querySelector(".article-links")
-        let articleLinksLi = document.createElement("li")
-        articleLinksLi.innerHTML = `${dataArr[index].title}`
-
-        articleLinks.style.display = "block";
-        articleLinks.insertAdjacentElement("afterend", articleContainer)
-        articleContainer.appendChild(articleTitle)
-        articleContainer.appendChild(articleDate)
-        articleContainer.appendChild(articleImage)
-        articleContainer.appendChild(articleSummary)
-        articleLinksUl.appendChild(articleLinksLi)
-        window.scrollTo(0, 0)
-
-
-    };
-});
-
-const burgerMenu = document.querySelector("#nav-icon1")
-const mobileNav = document.querySelector(".mobile-nav")
-const links = document.querySelectorAll(".mobile-links")
-
-burgerMenu.addEventListener("click", function () {
-    mobileNav.classList.toggle("active")
-    burgerMenu.classList.toggle("open")
-})
-
-links.forEach(item => {
-    item.addEventListener("click", () => {
-        burgerMenu.classList.toggle("open")
-        mobileNav.classList.toggle("active")
-    })
-})
 
 renderCards()
--- utils/utils.js
+++ utils/utils.js
@@ -6,6 +6,7 @@ const data = await getData("./obj.json");
 
 const mockAPI = { data }
 
+//render the article cards with API
 const renderCards = () => {
     let dataArr = mockAPI.data.posts
 
@@ -38,4 +39,74 @@ const renderCards = () => {
 
 }
 
-export { mockAPI, renderCards }
+const burgerMenu = document.querySelector("#nav-icon1")
+const mobileNav = document.querySelector(".mobile-nav")
+const links = document.querySelectorAll(".mobile-links")
+
+//open and close nav menu
+const navEventListeners = burgerMenu.addEventListener("click", function () {
+    mobileNav.classList.toggle("active")
+    burgerMenu.classList.toggle("open")
+})
+
+//when nav menu links are clicked, close menu andn toggle burger
+const linksEventListeners = links.forEach(item => {
+    item.addEventListener("click", () => {
+        burgerMenu.classList.toggle("open")
+        mobileNav.classList.toggle("active")
+    })
+})
+
+// when article title is clicked, render each article and display under nav
+const renderArticle = document.body.addEventListener('click', function (event) {
+    console.log(event.target.id)
+    let index = (event.target.id) - 1
+
+    if (event.target.className === 'title-link') {
+
+        let cardContainer = document.querySelector(".card-container")
+        let header = document.querySelector("header")
+        let container = document.querySelector(".container")
+        container.removeChild(cardContainer)
+        container.removeChild(header)
+
+        let dataArr = mockAPI.data.posts
+        console.log(dataArr[index])
+
+        let articleContainer = document.createElement("div")
+        articleContainer.classList.add("article-container")
+
+        let articleImage = document.createElement("div")
+        articleImage.classList.add("article-image")
+        articleImage.innerHTML = `<img src="./assets/${event.target.id}.jpeg">`
+
+        let articleTitle = document.createElement("div")
+        articleTitle.classList.add("article-title")
+        articleTitle.innerHTML = `<h3> ${dataArr[index].title} </h3>`
+
+        let articleSummary = document.createElement("div")
+        articleSummary.classList.add("article-summary")
+        articleSummary.innerHTML = `<p> ${dataArr[index].article} </p>`
+
+        let articleDate = document.createElement("p")
+        articleDate.classList.add("article-date")
+        articleDate.innerHTML = "June 2, 2022"
+        let articleLinksUl = document.querySelector("#article-ul")
+        let articleLinks = document.querySelector(".article-links")
+        let articleLinksLi = document.createElement("li")
+        articleLinksLi.innerHTML = `${dataArr[index].title}`
+
+        articleLinks.style.display = "block";
+        articleLinks.insertAdjacentElement("afterend", articleContainer)
+        articleContainer.appendChild(articleTitle)
+        articleContainer.appendChild(articleDate)
+        articleContainer.appendChild(articleImage)
+        articleContainer.appendChild(articleSummary)
+        articleLinksUl.appendChild(articleLinksLi)
+        window.scrollTo(0, 0)
+    };
+});
+
+
+
+export { mockAPI, renderCards, navEventListeners, linksEventListeners, renderArticle }
